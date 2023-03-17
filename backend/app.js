9a44ff9d82319cddd5992cc2c0bd141841558cab
Mehmet Can Türkmen <cturkmen@sabanciuniv.edu>
2023-03-17 21:43:32 +0300

    Added places and error routes.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const placesRoutes = require("./routes/places");
 
 const app = express();
 
-app.use(placesRoutes);
+const HttpError = require("./models/http-error");
+
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500).json({
+    message: error.message || "An Unknown Error Occured!",
+  });
+});
 
 app.listen(5000);
--- backend/routes/places.js
+++ backend/routes/places.js
@@ -1,9 +1,46 @@
-const express = require('express');
+const express = require("express");
+const HttpError = require("../models/http-error");
 const router = express.Router();
 
+const DUMMY_PLACES = [
+  {
+    pid: "p1",
+    uid: "u1",
+    title: "Empire States Building",
+    description: "An iconic building in New York",
+  },
+];
+
 router.get("/", (req, res, next) => {
-    console.log("GET request in places");
-    res.json({message: "It Works!"});
+  console.log("GET request in places");
+  res.json({ message: "It Works!" });
+});
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.pid === placeId);
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find the place with the specified place ID", 404)
+    );
+  }
+
+  res.json({ place: place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.uid === userId);
+
+  if (!place) {
+    throw new HttpError(
+      "Could not find the place with the specified user ID",
+      404
+    );
+  }
+
+  res.json({ place: place });
 });
 
 module.exports = router;
