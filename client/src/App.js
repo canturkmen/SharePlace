9ea2e1bb67d1ec877965229b09cf26957b8d1384
Mehmet Can Turkmen <cturkmen@sabanciuniv.edu>
2023-06-21 21:36:47 +0300

    Added auto log-out and completed auto log-in.

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,9 @@ import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import AuthPage from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
-
 import Users from "./user/pages/Users";
+
+let logoutTimer;
 
 const unloggedRouter = createBrowserRouter([
   {
@@ -55,15 +56,20 @@ const loggedRouter = createBrowserRouter([
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       "userData",
       JSON.stringify({
         userId: uid,
         token: token,
+        expiration: tokenExpirationDate.toISOString(),
       })
     );
   }, []);
@@ -71,15 +77,36 @@ const App = () => {
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
     localStorage.removeItem("userData");
   }, []);
 
+  // Auto Log-In (When we refresh the page, we make sure that we log-in the user if there is a token)
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
-    if(storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
+
+  // Auto Log-Out (When the token expires, we make sure that the user is logged out)
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, tokenExpirationDate, logout]);
 
   let element;
   if (token) {
